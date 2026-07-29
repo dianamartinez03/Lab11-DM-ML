770a0267c64e1e2b9b939a14b13a56b954c5d69c
dianamartinez03 <dianamartinez0323@gmail.com>
2026-07-29 12:34:29 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -45,12 +45,12 @@ class TestCalculator(unittest.TestCase):
 
      def test_hypotenuse(self): # 3 assertions
          self.assertNotEqual((a,b), (0,0)
-         self.assertEqual(hypotenuse(5,12),13)
+         self.assertEqual(hypotenuse(5,12),13.0)
          self.assertTrue(hypotenuse(5.0,12.0) == 13.0)
         
 
      def test_sqrt(self): # 3 assertions
-         self.assertTrue(sqrt(25) == 5)
+         self.assertTrue(sqrt(25) == 5.0)
          self.assertFalse(sqrt(36), 3)
          self.assertNotEqual((a), (-4))
          

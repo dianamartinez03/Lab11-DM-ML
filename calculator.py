96309c63a6de27c6ddcbae9e83f235fbe720d9be
dianamartinez03 <dianamartinez0323@gmail.com>
2026-07-29 13:16:48 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,16 +10,16 @@ import math
 def add(a, b): 
     return a + b
 
-def sub(a,b):
+def subtract(a,b):
     return a - b
 
-def mult(a,b):
+def mul(a,b):
     return a * b
 
 def div(a,b):
     return a / b
 
-def sqrt(a):
+def square_root(a):
     return math.sqrt(a)
 
 def logarithm(a,b):

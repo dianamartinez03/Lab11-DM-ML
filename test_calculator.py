d285809b446d3b8554eddd8aa90953e87c263ddb
dianamartinez03 <dianamartinez0323@gmail.com>
2026-07-29 12:38:26 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import add,sub,div,mult,sqrt,logarithm
+from calculator import add,sub,div,mult,sqrt,logarithm,hypotenuse
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
